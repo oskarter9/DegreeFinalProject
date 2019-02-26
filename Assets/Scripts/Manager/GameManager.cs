f27730dbd32568821aaad4ca5fea35f6c57e9ea3
oskarter9 <oscar5teruel@gmail.com>
2019-02-26 18:46:42 +0100

    Tobacco Machine and GameManager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    public int currentPuzzle;
+
+    private bool _gameFinished;
+    private ReferencesManager _referencesManager;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        _referencesManager = GetComponent<ReferencesManager>();
+        currentPuzzle = 1;
+        SetPuzzleEnvironment(currentPuzzle);
+    }
+
+    public void SetPuzzleEnvironment(int currentPuzzle)
+    {
+        switch (currentPuzzle)
+        {
+            case 1:
+                _referencesManager.POneControllerContainer.AddComponent<PuzzleOneController>();
+                break;
+            case 2:
+                Debug.Log("Has pasado a la segunda parte");
+                break;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Manager/ReferencesManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferencesManager : MonoBehaviour
+{
+    public static ReferencesManager instance = null;
+
+    public Light[] POneLights;
+    public GameObject POneLightSwitch;
+    public GameObject POneControllerContainer;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
