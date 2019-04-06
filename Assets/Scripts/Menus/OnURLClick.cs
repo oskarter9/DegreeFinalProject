692ed566e8d876ca033cdb2db03dad25cee610b2
oskarter9 <oscar5teruel@gmail.com>
2019-04-06 13:31:06 +0200

    Fungus imported and Menus base done

---
+++ Assets/Scripts/Menus/OnURLClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnURLClick : MonoBehaviour {
+
+    public string URL;
+
+    public void GoUrl()
+    {
+        Application.OpenURL(URL);
+    }
+	
+}
---
+++ Assets/Scripts/Menus/SplashScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SplashScreenManager : MonoBehaviour {
+
+    [Header("RESOURCES")]
+    public GameObject mainPanels;
+    private Animator mainPanelsAnimator;
+
+    [Header("SETTINGS")]
+    public bool disableSplashScreen;
+
+    void Start ()
+    {
+        if (disableSplashScreen == true)
+        {
+            mainPanels.SetActive(true);
+
+            mainPanelsAnimator = mainPanels.GetComponent<Animator>();
+            mainPanelsAnimator.Play("Main Panel Opening");
+        }
+    }
+}
