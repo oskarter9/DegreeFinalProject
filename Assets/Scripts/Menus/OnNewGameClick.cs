d04bc0bf4b9f3e5fa95679e60a0aa9a1a27ac9d0
oskarter9 <oscar5teruel@gmail.com>
2019-04-08 14:07:18 +0200

    Fade In to the scene done

--- Assets/Scripts/Menus/OnNewGameClick.cs
+++ Assets/Scripts/Menus/OnNewGameClick.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OnNewGameClick : MonoBehaviour {
-    
+
+    public Animator SceneChangerAC;
+
+    private int _sceneToGo;
+    private bool _ableToLoad;
+
     public void LoadNewGame(int buildIndex)
     {
+        _sceneToGo = buildIndex;
         PlayerPrefs.SetInt("SomethingToLoad", 0);
-        StartCoroutine(LoadSceneAsync(buildIndex));
+        SceneChangerAC.SetTrigger("FadeOut");
     }
 
     private IEnumerator LoadSceneAsync(int buildIndex)
     {
-        AsyncOperation async = SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Additive);
-
-        while (!async.isDone)
+        if (_ableToLoad)
         {
-            yield return null;
+            AsyncOperation async = SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Additive);
+
+            while (!async.isDone)
+            {
+                yield return null;
+            }
+
+            AsyncOperation async2 = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+            while (!async2.isDone)
+                yield return null;
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneAt(buildIndex));
         }
+    }
 
-        AsyncOperation async2 = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
-
-        while (!async2.isDone)
-            yield return null;
-
-        SceneManager.SetActiveScene(SceneManager.GetSceneAt(buildIndex));
+    public void OnFadeOut()
+    {
+        _ableToLoad = true;
+        StartCoroutine(LoadSceneAsync(_sceneToGo));
     }
 }
---
+++ Assets/Scripts/Menus/UIElementSound.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIElementSound : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+{
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    private bool enableHoverSound;
+    private bool enableClickSound;
+
+    private AudioSource HoverSource { get { return GetComponent<AudioSource>(); } }
+    private AudioSource ClickSource { get { return GetComponent<AudioSource>(); } }
+
+    void Start()
+    {
+        if (GetComponent<Button>().interactable)
+        {
+            enableHoverSound = true;
+            enableClickSound = true;
+        }
+        gameObject.AddComponent<AudioSource>();
+        HoverSource.clip = hoverSound;
+        ClickSource.clip = clickSound;
+
+        HoverSource.playOnAwake = false;
+        ClickSource.playOnAwake = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(enableHoverSound == true)
+        {
+            HoverSource.PlayOneShot(hoverSound);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (enableClickSound == true)
+        {
+            ClickSource.PlayOneShot(clickSound);
+        }
+    }
+}
